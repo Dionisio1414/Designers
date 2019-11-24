0b536223cbf7f459bfb83368809f8ff48e9a3653
Denys Soloviev <dionisio14solo14@gmail.com>
2019-11-24 23:55:50 +0200

    full desktop ver. without search button functional

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+	let headerInformationHeight = document.querySelector('.header__information').offsetHeight,
+		headerBottomMenu = document.querySelector('.header__bottom');
+	
+	
 	const homeSlider = tns({
 		container: document.querySelector(".home-slider"),
 		items: 1,
@@ -6,5 +10,11 @@ document.addEventListener('DOMContentLoaded', () => {
 		speed: 400,
 		"mouseDrag": true,
 		controls: false
-	})
+	});
+	
+	window.addEventListener("scroll", () => {
+		let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		if(scrollTop > headerInformationHeight) headerBottomMenu.classList.add('fixed')
+		else headerBottomMenu.classList.remove('fixed');
+	});
 });
